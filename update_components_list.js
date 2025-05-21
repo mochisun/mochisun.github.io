7030e8d3ea0bc59a9e547960d24298b591191eeb
mochisun <mochisun@lastSem.>
2025-05-20 20:34:33 -0700

    upadated component/tool listing

--- update_components_list.js
+++ update_components_list.js
@@ -1,12 +1,36 @@
-//const list = document.getElementById('component-list');
 
-//grab .txt list info
-fetch('components_list.txt')
-.then(response => response.text())
-.then(text =>{
-    //split the received text into array
-    const lines = text.split(/\r?\n/);
-    lines.forEach((line, index) => {
-        console.log(line);
-    })
-})
+//list off all mechanic knowledge bullets as strings?
+const semiTools = ` - Helium Leak Detector
+ - Alum-a-Lift (raise/lower chemical Showerheads)
+ - Pneumatic Lift Pistons
+ - Generic Mechanical Tools (Torque wrenches, bits/sockets, wire cutters, etc.)
+ - Multimeter
+ - Digital Calipers (measure pin height)
+ - Laser Alignment Lids
+ - Micrometers (leveling/centering)
+ - Vacuum Pumps
+ - LOTO kits
+ - TLD (Toxic Leak Detector)`;
+const aviTools = ` - Radar Test Equipment
+ - Fire Extinguisher
+ - Forklift
+ - Jammer Vehicle (to install targeting pods on pylons)
+ - Chaff/flare testers
+ - Safety wire pliers
+ - Borescope
+ - Fuel quantity test set `;
+const compKnowledge = ` - PLC (Programmable Logic controllers)
+ - Interlocks (Software/Hardware)
+ - Digital/Analog I/Os(inputs/outputs)
+ - Pressure transducers
+ - Differential pressure sensors
+ - Throttle valves
+ - Thermocouples
+ - Gas lines
+ - Pneumatic/Hydraulic operations
+ - Parts ordering/inventory`;
+
+//set html div elements
+document.getElementById('semiTools').textContent = semiTools;
+document.getElementById('aviTools').textContent = aviTools;
+document.getElementById('compKnowledge').textContent = compKnowledge;
